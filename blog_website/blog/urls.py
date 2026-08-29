1e6282f05d849a76e3994b1a43868f4317c7e50c
anoopungrd <techanoop007@gmail.com>
2026-08-29 13:06:45 +0200

    Adding the updated files to the repo for class based views

--- blog_website/blog/urls.py
+++ blog_website/blog/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
-from .views import post_list, post_detail
+
+# from .views import post_list, post_detail
+from .views import BlogListView, BlogDetailView, BlogCreateView,BlogUpdateView,BlogDeleteView  # new
 
 urlpatterns = [
-    path("post/<int:pk>/", post_detail, name="post_detail"),
-    path("", post_list, name="home"),
+    # path("post/<int:pk>/", post_detail, name="post_detail"),
+    # path("", post_list, name="home"),
+    path("post/new/", BlogCreateView.as_view(), name="post_new"),  # new
+    path("post/<int:pk>/", BlogDetailView.as_view(), name="post_detail"),  # new
+    path("post/<int:pk>/edit/", BlogUpdateView.as_view(), name="post_edit"), # Added for UpdateView
+    path("post/<int:pk>/delete/", BlogDeleteView.as_view(), name="post_delete"), # Added for DeleteView
+    path("", BlogListView.as_view(), name="home"),  # new
 ]
